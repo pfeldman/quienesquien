627c415d12765b1936aa07cf8f8e138edd726bef
Pablo Feldman <pfeldman@makingsense.com>
2018-01-20 16:26:31 -0300

    Fix Problem

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var app = require('express').createServer();
-app.get('/',function(req,res) {
-    res.send("Hello World");
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+    res.send('Hello World!');
 });
-
-app.listen(process.env.PORT || 3000,function(){
-    console.log("listening on 3000");
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
 });
