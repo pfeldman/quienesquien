c56f15e424204511ca0b13290cfb95ec8106b1ba
Pablo Feldman <pfeldman@makingsense.com>
2018-01-20 19:49:44 -0300

    Include Babel to use ES6

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ var express = require('express');
 var app = express();
 var connect = require('./db');
 
-
 app.get('/', function (req, res) {
     res.send('Hello World!');
-    console.log(connect());
 });
 
 app.listen(process.env.PORT || 3000, function () {
+    connect();
     console.log('Example app listening on port 3000!');
 });
