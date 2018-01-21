1fa4a9d08f4c549dd50c251a63ebe719df3f4f32
Pablo Feldman <pfeldman@makingsense.com>
2018-01-20 23:59:30 -0300

    Set headers

--- app.js
+++ app.js
@@ -59,6 +59,8 @@ class App {
 
     boot() {
         app.get('/game', (req, res) => {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
             this.game()
                 .then(game => res.send(JSON.stringify(game)))
         })
